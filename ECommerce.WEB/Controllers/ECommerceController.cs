2455d4d82652c75b6bda14ba141dc07a1d726fe5
Faru Nuri Sönmez <info@farunurisonmez.com>
2024-10-10 14:37:09 +0300

    Fix issue #7

---
+++ ECommerce.WEB/Controllers/ECommerceController.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.WEB.Services;
+using ECommerce.WEB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WEB.Controllers
+{
+    public class ECommerceController : Controller
+    {
+        private readonly ECommerceService service1;
+
+        public ECommerceController(ECommerceService service) {
+            service1 = service;
+        }
+
+        public async Task<IActionResult> Search([FromQuery] SearchPageViewModel searchPageView)
+        {
+            var (eCommerceList, totalCount, pageLinkCount) = await service1.SearchAsync(
+                searchPageView.SearchViewModel,
+                searchPageView.Page,
+                searchPageView.PageSize);
+
+            return View(searchPageView);
+        }
+    }
+}
---
+++ ECommerce.WEB/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ECommerce.WEB.ViewModels
+{
+    public class SearchPageViewModel
+    {
+        public long TotalCount { get; set; }
+        public int Page {  get; set; }
+        public int PageSize { get; set; }
+        public long PageLinkCount { get; set; }
+        public List<ECommerceViewModel> List { get; set; }
+        public ECommerceSearchViewModel SearchViewModel { get; set; }
+
+        public string CreatePageUrl(HttpRequest request, int page, int pageSize)
+        {
+            var currentUrl = new Uri($"{request.Scheme}://{request.Host}{request.Path}{request.QueryString}").AbsoluteUri;
+            if(currentUrl.Contains("page", StringComparison.OrdinalIgnoreCase))
+            {
+                currentUrl = currentUrl.Replace($"Page={Page}", $"Page={page}", StringComparison.OrdinalIgnoreCase);
+                currentUrl = currentUrl.Replace($"PageSize={PageSize}", $"Page={pageSize}", StringComparison.OrdinalIgnoreCase);
+            
+            } 
+            else {
+                currentUrl = $"{currentUrl}?Page={page}";
+                currentUrl = $"{currentUrl}?PageSize={pageSize}";
+            }
+
+            return currentUrl;
+        }
+    }
+}

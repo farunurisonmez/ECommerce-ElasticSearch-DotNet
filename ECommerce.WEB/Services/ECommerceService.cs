216528b165f50a449ca9d9333acded85174f2b50
Faru Nuri Sönmez <info@farunurisonmez.com>
2024-10-10 11:15:26 +0300

    Fix issue #6

---
+++ ECommerce.WEB/Services/ECommerceService.cs
@@ -0,0 +1,32 @@
+﻿using ECommerce.WEB.Repositories;
+using ECommerce.WEB.ViewModels;
+
+namespace ECommerce.WEB.Services
+{
+    public class ECommerceService
+    {
+        private readonly ECommerceRepository _repository;
+
+        public ECommerceService(ECommerceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<(List<ECommerceViewModel>, long totalCount, long pageLinkCount)> SearchAsync(ECommerceSearchViewModel searchModel, int page, int pageSize)
+        {
+            var (eCommerceList, totalCount) = await _repository.SearchAsync(searchModel, page, pageSize);
+
+            var pageLinkCountCalc = totalCount % pageSize;
+            long pageLinkCount = pageLinkCountCalc == 0 ? totalCount / pageSize : (totalCount / pageSize) + 1;
+
+            var eCommerceListViewModel = eCommerceList.Select(x => new ECommerceViewModel()
+            {
+                Id = x.Id,
+                Category = String.Join(",",x.Category),
+
+            }).ToList();
+
+            return (eCommerceListViewModel, totalCount, pageLinkCount);
+        } 
+    }
+}
---
+++ ECommerce.WEB/ViewModels/ECommerceViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using System.Text.Json.Serialization;
+namespace ECommerce.WEB.ViewModels
+{
+    public class ECommerceViewModel
+    {
+        public string? Id { get; set; } = null!;
+
+        public string? Category { get; set; } = null!;
+
+    }
+}

1b7801472aa326fabbfb452680e0d4079234d780
Faru Nuri Sönmez <info@farunurisonmez.com>
2024-10-10 08:52:34 +0300

    Fix issue #5

---
+++ ECommerce.WEB/ViewModels/ECommerceSearchViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ECommerce.WEB.ViewModels
+{
+    public class ECommerceSearchViewModel
+    {
+        [Display(Name = "Category")]
+        public string? Category { get; set; }
+
+        [Display(Name = "Gender")]
+        public string? Gender { get; set; }
+
+        [Display(Name = "Order Date (Start)")]
+        [DataType(DataType.Date)]
+        public DateTime? OrderDateStart { get; set; }
+
+        [Display(Name = "Order Date(End)")]
+        [DataType(DataType.Date)]
+        public DateTime? OrderDateEnd { get; set; }
+        public string? CustomerFullName { get; set; }
+        
+    }
+}
